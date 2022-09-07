b76a7948292b6dac2fe115cff470a49902f81a98
adamdasovich <adamdasovich@gmail.com>
2022-09-07 11:52:26 -0400

    Updates

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 let image = document.getElementById('myImage')
+let colorButton = document.getElementById('color-button')
+let wheelButton = document.getElementById('crazy-button')
 
 function blooming() {
-	image.src.match('dog.png') ? image.src = 'dog_grey2.png' : image.src = 'dog.png'
+	image.src.match('dog.png') ? image.src = './images/THC-CHAIN.png' : image.src = './images/dog.png'
 }
 image.addEventListener('click', blooming)
+
+
