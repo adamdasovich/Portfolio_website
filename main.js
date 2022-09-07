dcdd350bfecda2f222ae3c015d1c489de0df934f
adamdasovich <adamdasovich@gmail.com>
2022-09-07 14:12:31 -0400

    Updates

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
-let image = document.getElementById('myImage')
-let colorButton = document.getElementById('color-button')
-let wheelButton = document.getElementById('crazy-button')
+let image = document.getElementById('bear')
+const rotateLeftButton = document.getElementById('rotate-left')
+const rotateRightButton = document.getElementById('rotate-right')
+
+let rotation = 0
 
 function blooming() {
-	image.src.match('dog.png') ? image.src = './images/THC-CHAIN.png' : image.src = './images/dog.png'
+	image.src.match('Heart.png') ? image.src = './images/bear.png' : image.src = './images/Heart.png'
 }
 image.addEventListener('click', blooming)
 
 
+function rotate(element, event) {
+	if (event.target.id === 'rotate-left') {
+		rotation - 15
+	} else {
+		rotation + 15
+	}
+	element.style.transform = `rotate(${rotation}deg)`
+}
